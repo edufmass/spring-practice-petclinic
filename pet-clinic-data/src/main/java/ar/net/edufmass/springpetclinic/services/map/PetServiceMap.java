a80624d588c91ac32041f8b7ee0e47891b0bbee9
edufmass <git@edufmass.ar>
2023-09-22 11:48:41 -0300

    Added boostrap data. Closes #14.

--- pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package ar.net.edufmass.springpetclinic.services.map;
 
 import ar.net.edufmass.springpetclinic.model.Pet;
-import ar.net.edufmass.springpetclinic.services.CrudService;
+import ar.net.edufmass.springpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package ar.net.edufmass.springpetclinic.services.map;
 
 import ar.net.edufmass.springpetclinic.model.Vet;
-import ar.net.edufmass.springpetclinic.services.CrudService;
+import ar.net.edufmass.springpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
