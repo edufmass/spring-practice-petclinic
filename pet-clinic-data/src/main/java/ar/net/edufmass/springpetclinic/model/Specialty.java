75365a4ab597293de56afcea82b5ef802392af71
edufmass <git@edufmass.ar>
2024-02-29 10:57:07 -0300

    Added Specialty entity and updated Vet entity. Closes #27.

---
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package ar.net.edufmass.springpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
