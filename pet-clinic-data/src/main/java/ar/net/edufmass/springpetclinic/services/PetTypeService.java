5ec044b108e47f44392c5e5de3f276d2ee52fb8e
edufmass <git@edufmass.ar>
2024-02-29 11:10:37 -0300

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ar.net.edufmass.springpetclinic.services;
+
+import ar.net.edufmass.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
