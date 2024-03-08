d21ff6a458e29b99d1b997d54f345898e0bc168a
edufmass <git@edufmass.ar>
2024-03-08 16:01:06 -0300

    Implemented JPA annotation on Vet and Specialty. Closes #37.

--- pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package ar.net.edufmass.springpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package ar.net.edufmass.springpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "Vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "VetSpecialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
