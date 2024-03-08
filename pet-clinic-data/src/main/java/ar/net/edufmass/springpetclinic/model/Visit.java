b9e732e1c60aa7a2404d65f0f676640c36efb750
edufmass <git@edufmass.ar>
2024-03-08 18:31:20 -0300

    Implemented JPA annotation on Visit. Closes #38.

--- pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ar/net/edufmass/springpetclinic/model/Visit.java
@@ -1,11 +1,23 @@
 package ar.net.edufmass.springpetclinic.model;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "PetVisits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
